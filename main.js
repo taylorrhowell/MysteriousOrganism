69e1f946ead70b42bf3c893fb8d42b5d55ce5cbc
Taylor Howell <66186140+taylorrhowell@users.noreply.github.com>
2020-11-11 15:56:04 -0800

    create factory funct, add properties + 1st method

--- main.js
+++ main.js
@@ -13,7 +13,27 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
-
+const pAequorFactory = (number, DNA) => {
+    return {
+        specimenNum: number,
+        dna: DNA,
+        mutate() {
+            const index = Math.floor(Math.random() * 16);
+            let mutantBase = this.dna[index];
+            let newBase = returnRandBase();
+            if (mutantBase !== newBase) {
+                this.dna.splice(index, 1, newBase);
+                return this.dna;
+            } else {
+                this.mutate();
+            };
+        }
+    };
+};
+const nokiDNA = ['C', 'A', 'T', 'G', 'C', 'A', 'T', 'G', 'C', 'A', 'T', 'G', 'C', 'A', 'T'];
+const noki = pAequorFactory(1, nokiDNA);
+console.log(noki.dna);
+console.log(noki.mutate());
 
 
 
